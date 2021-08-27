f11d48d2959dbe00efaaf14353324659f10d3b51
Rafi Blecher <rafiblecher@gmail.com>
2021-08-27 19:58:36 +1000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d5321c71ebcb827064322ae7c675d94'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '02207b3c-152e-47ab-beb3-6aa214849f1c'
+};
